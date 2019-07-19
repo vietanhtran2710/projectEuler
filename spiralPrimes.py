1d64b514723bc3089dee35707e1de0e77513758e
vietanhtran2710 <vietanhtran.uet@gmail.com>
2019-07-19 12:36:13 +0700

    Python solution

--- spiralPrimes.py
+++ spiralPrimes.py
@@ -6,17 +6,13 @@ def isPrime(value):
     return True
 
 
-length, start, countPrime, spiralLength, sideLength = 2, 1, 1, 1, 1
-while countPrime / spiralLength >= 0.1:
+length, start, countPrime, spiralLength, sideLength = 2, 1, 0, 1, 1
+while countPrime == 0 or countPrime / spiralLength >= 0.1:
     sideLength += 2
+    spiralLength += 4
     for i in range(4):
-        spiralLength += 1
         start += length
         if isPrime(start):
             countPrime += 1
-        if countPrime / spiralLength < 0.1:
-            print(sideLength,"--")
-            break
-    if countPrime / spiralLength < 0.1:
-        break
     length += 2
+print(sideLength)
